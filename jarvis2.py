ae55f3ce0c58ca98a4f4f4df37dffb2cb61f4301
om <165636504+lovmor@users.noreply.github.com>
2024-08-12 22:48:59 +0530

    Update jarvis2.py

--- jarvis2.py
+++ jarvis2.py
@@ -17,10 +17,20 @@ engine.setProperty('voice', voices[0].id)
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
+    
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
 
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
 
-      
+    else:
+        speak("Good Evening!")  
 
+    speak("I am Jarvis Sir. Please tell me how may I help you")
+    
 def takeCommand():
     #It takes microphone input from the user and returns string output
 
